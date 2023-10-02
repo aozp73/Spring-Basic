9647edc86cf99d84601140990ed22d16339d9211
aozp73 <aozp73@naver.com>
2023-10-02 15:21:21 +0900

    주문,할인 - 새로운 정책 적용

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -5,7 +5,7 @@ import hello.core.member.Member;
 
 public class RateDiscountPolicy implements DiscountPolicy{
 
-    private int discountPercent = 10;
+    private int discountPercent = 10; // 10% 할인
 
     @Override
     public int discount(Member member, int price) {
